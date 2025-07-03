ea4add6245c64f66fcab49d5814145417c793096
RameexZaidi <rameezzaidi12@outlook.com>
2025-07-03 15:04:38 +0500

    Update Real_Estate.js

--- pages/Real_Estate.js
+++ pages/Real_Estate.js
@@ -1,81 +1,3 @@
-// pages/index.js
-import { useState, useEffect } from "react";
-import { Oval } from "react-loader-spinner"; // Import the loader
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import RealstateSlider from "@/components/RealstateSlider";
-import RealstateFeature from "@/components/RealstateFeature";
-import TopBid from "@/components/TopBid";
-import RealstateCategory from "@/components/RealstateCategory";
-import Realstate from "@/components/realstate";
-import Real_Estate from "../components/Real_Estate";
-//import { useSession } from "next-auth/react"; // Assuming you're using NextAuth.js
-// import PopupSequence from "../components/PopupSequence";
-
-
-export default function Home() {
-  //const { data: session, status } = useSession(); // Get user session
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true); // State to handle loading
-  
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch(
-          "https://admin.xpertbid.com/api/get-realestate"
-        );
-        const data = await response.json();
-        setProducts(data.product || []);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      } finally {
-        setLoading(false); // Stop loading after fetch
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
-
-// if (loading) {
-//   return (
-//     <div className="loading-container">
-//       <Oval color="#000" height={50} width={50} />
-//     </div>
-//   );
-// }
-  return (
-    
-    <>
-      {loading ? (
-        // Show loader while loading
-        <div className="loader-container">
-          <Oval
-            height={80}
-            width={80}
-            color="#3498db"
-            secondaryColor="#f3f3f3"
-            ariaLabel="loading-indicator"
-          />
-        </div>
-      ) : (
-        <>
-          {/* {isFirstLogin && <p className="welcome-message"> Welcome! This is your first login.</p>} */}
-        
-          <Real_Estate></Real_Estate>
-        </>
-      )}
-      <style jsx>{`
-        .loader-container {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100vh;
-        }
-      `}</style>
-    </>
-  );
-}
 import React from 'react';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -300,4 +222,4 @@ const Real_Estate = () => {
     );
 };
 
-
+export default Real_Estate;
